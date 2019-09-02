0c3116703e66b4fb8fa95ce1a0c904a1657cd21f
Okinan <okinan@chiru.no>
2019-09-02 00:58:08 -0500

    ENTERPRISE PROGRAMMING

--- etc/nixos/boot.nix
+++ etc/nixos/boot.nix
@@ -8,15 +8,22 @@ in {
 		kernelPackages = pkgs.linuxPackages_latest;
 		kernelModules = [ "nct6775" ];
 
-		kernelPatches = lib.singleton {
-			name = "enable-vfio";
-			patch = null;
+		kernelPatches = [
+			{
+				name = "enable-vfio";
+				patch = null;
 
-			extraConfig = ''
-				VFIO y
-				VFIO_PCI y
-			'';
-		};
+				extraConfig = ''
+					VFIO y
+					VFIO_PCI y
+				'';
+			}
+
+			{
+				name = "amd-fuckup-kernel-patch";
+				patch = /etc/nixos/amd_fuckup_kernel_patch.diff;
+			}
+	];
 
 		kernelParams = [
 			"amd_iommu=on"
