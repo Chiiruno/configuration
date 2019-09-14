70b8b55f18a3b48206d0efff419dc10392654975
Okinan <okinan@chiru.no>
2019-09-14 00:48:17 -0500

    cleanup

--- etc/nixos/override.nix
+++ etc/nixos/override.nix
@@ -2,22 +2,11 @@ let
 	baseConfig = { allowUnfree = true; };
 	unstable = import <nixos-unstable> { config = baseConfig; };
 in {
-	imports = [
-		<nixos-unstable/nixos/modules/services/databases/postgresql.nix>
-		<nixos-unstable/nixos/modules/services/web-servers/hydron.nix>
-		<nixos-unstable/nixos/modules/services/networking/zeronet.nix>
-	];
-
-	disabledModules = [
-		"services/databases/postgresql.nix"
-		"services/web-servers/hydron.nix"
-		"services/networking/zeronet.nix"
-	];
+	imports = [ <nixos-unstable/nixos/modules/services/networking/zeronet.nix> ];
+	disabledModules = [ "services/networking/zeronet.nix" ];
 
 	nixpkgs.config = baseConfig // {
 		packageOverrides = pkgs: {
-			postgresql = unstable.postgresql;
-			hydron = unstable.hydron;
 			zeronet = unstable.zeronet;
 			multimc = unstable.multimc;
 		};
